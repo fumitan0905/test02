2b0cf63c076ffb8dd3886919db1d8f57413fd019
id <id@AQ-0103>
2023-06-21 16:18:29 +0900

    更新のテスト０１

--- src/test02/Sample01.java
+++ src/test02/Sample01.java
@@ -5,6 +5,9 @@ public class Sample01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("こんにちは！");
+		
+		System.out.println("16:17　一回更新しました。");
+
 	}
 
 }
