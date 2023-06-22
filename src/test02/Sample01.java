6992fa6f4c67c4cd2222aec3531885c143bb28d6
id <id@AQ-0103>
2023-06-22 09:32:22 +0900

    xxx テストします

--- src/test02/Sample01.java
+++ src/test02/Sample01.java
@@ -1,7 +1,6 @@
 package test02;
 
 public class Sample01 {
-
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("こんにちは！");
@@ -11,6 +10,9 @@ public class Sample01 {
 		System.out.println("こんにちは山中康次です");
 		
 		System.out.println("xxx 一回テストします");
+		
+		System.out.println("xxx 木曜　09:31 テストします");
+
 	}
 
 }
